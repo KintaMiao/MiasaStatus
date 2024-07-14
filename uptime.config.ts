b4d3a050ed1e39ddd1282087042a7b3a1b153702
KintaMiao <45995071+KintaMiao@users.noreply.github.com>
2024-07-14 11:25:31 +0800

    restore

--- uptime.config.ts
+++ uptime.config.ts
@@ -1,61 +1,40 @@
 const pageConfig = {
   // Title for your status page
-  title: "lyc8503's Status Page",
+  title: "Miasa's Status Page",
   // Links shown at the header of your status page, could set `highlight` to `true`
   links: [
-    { link: 'https://github.com/lyc8503', label: 'GitHub' },
-    { link: 'https://blog.lyc8503.net/', label: 'Blog' },
-    { link: 'mailto:me@lyc8503.net', label: 'Email Me', highlight: true },
+    { link: 'https://kintamiao.top', label: 'Blog' },
+    { link: 'https://github.com/KintaMiao', label: 'GitHub' },
+    { link: 'mailto:mail@kintamiao.top', label: 'Email Me', highlight: true },
   ],
 }
 
 const workerConfig = {
   // Write KV at most every 3 minutes unless the status changed.
-  kvWriteCooldownMinutes: 3,
+  kvWriteCooldownMinutes: 10,
   // Define all your monitors here
   monitors: [
-    // Example HTTP Monitor
     {
-      // `id` should be unique, history will be kept if the `id` remains constant
-      id: 'foo_monitor',
-      // `name` is used at status page and callback message
-      name: 'My API Monitor',
-      // `method` should be a valid HTTP Method
-      method: 'POST',
-      // `target` is a valid URL
-      target: 'https://example.com',
-      // [OPTIONAL] `tooltip` is ONLY used at status page to show a tooltip
-      tooltip: 'This is a tooltip for this monitor',
-      // [OPTIONAL] `statusPageLink` is ONLY used for clickable link at status page
-      statusPageLink: 'https://example.com',
-      // [OPTIONAL] `expectedCodes` is an array of acceptable HTTP response codes, if not specified, default to 2xx
-      expectedCodes: [200],
-      // [OPTIONAL] `timeout` in millisecond, if not specified, default to 10000
-      timeout: 10000,
-      // [OPTIONAL] headers to be sent
-      headers: {
-        'User-Agent': 'Uptimeflare',
-        Authorization: 'Bearer YOUR_TOKEN_HERE',
-      },
-      // [OPTIONAL] body to be sent
-      body: 'Hello, world!',
-      // [OPTIONAL] if specified, the response must contains the keyword to be considered as operational.
-      responseKeyword: 'success',
-      // [OPTIONAL] if specified, the check will run in your specified region,
-      // refer to docs https://github.com/lyc8503/UptimeFlare/wiki/Geo-specific-checks-setup before setting this value
-      checkLocationWorkerRoute: 'https://xxx.example.com',
+      id: 'miasa_monitor',
+      name: 'KintaMiao Website',
+      method: 'GET',
+      target: 'https://kintamiao.top',
+      tooltip: 'KintaMiao Website',
+      statusPageLink: '',
     },
-    // Example TCP Monitor
     {
-      id: 'test_tcp_monitor',
-      name: 'Example TCP Monitor',
-      // `method` should be `TCP_PING` for tcp monitors
+      id: 'yunyoo_hk_vps',
+      name: 'HK VPS',
       method: 'TCP_PING',
-      // `target` should be `host:port` for tcp monitors
-      target: '1.2.3.4:22',
-      tooltip: 'My production server SSH',
-      statusPageLink: 'https://example.com',
-      timeout: 5000,
+      target: '103.169.126.229:22',
+      tooltip: 'Miasa HK Server',
+    },
+    {
+      id: 'yunyoo_us_vps',
+      name: 'US VPS',
+      method: 'TCP_PING',
+      target: '208.87.240.5:10136',
+      tooltip: 'Miasa US Server',
     },
   ],
   notification: {
@@ -83,7 +62,6 @@ const workerConfig = {
     ) => {
       // This callback will be called when there's a status change for any monitor
       // Write any Typescript code here
-
       // This will not follow the grace period settings and will be called immediately when the status changes
       // You need to handle the grace period manually if you want to implement it
     },
@@ -99,6 +77,5 @@ const workerConfig = {
     },
   },
 }
-
 // Don't forget this, otherwise compilation fails.
 export { pageConfig, workerConfig }
